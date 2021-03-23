174c239ea4bc4d1615a8a5ea390e89279e2ca798
sankethsj <32950017+sankethsj@users.noreply.github.com>
2021-03-23 22:34:29 +0530

    Enter

--- indexrange/range.js
+++ indexrange/range.js
@@ -1,35 +1,71 @@
-function findrange() {
-    event.preventDefault();
-    var nif = document.getElementById('niftyVal').value;
-    var vi = document.getElementById('vix').value;
-    if (nif == "" || vi == "") {
-        alert("Please enter values on both fields!!!")
-    }
-    else {
-        nif = parseInt(nif);
-        vi = parseInt(vi);
-        volm = vi / Math.sqrt(12);
-        volw = vi / Math.sqrt(52);
-        vold = vi / Math.sqrt(365);
 
-        var ahigh, alow, mhigh, mlow, whigh, wlow, dhigh, dlow;
-        ahigh = parseInt(nif + ((nif * vi) / 100));
-        alow = parseInt(nif - ((nif * vi) / 100));
-        mhigh = parseInt(nif + ((nif * volm) / 100));
-        mlow = parseInt(nif - ((nif * volm) / 100));
-        whigh = parseInt(nif + ((nif * volw) / 100));
-        wlow = parseInt(nif - ((nif * volw) / 100));
-        dhigh = parseInt(nif + ((nif * vold) / 100));
-        dlow = parseInt(nif - ((nif * vold) / 100));
+var option = document.getElementById('index').value;
+console.log(option);
 
-
-        document.getElementById('result').innerHTML = "Daily High/Low  = " + dhigh + " - " + dlow + "<br><br>" +
-            "Weekly High/Low  = " + whigh + " - " + wlow + "<br><br>" +
-            "Monthly High/Low = " + mhigh + " - " + mlow + "<br><br>" +
-            "Annual High/Low  = " + ahigh + " - " + alow;
-    }
-    return false
+function changeoi(that){
+    runoi(that.value);
 }
 
-fetch('https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY')
-  .then(json => console.log(json));
+function runoi(option) {
+    const url = 'https://api.niftytrader.in/api/FinNiftyOI/niftyoilistData?reqType=' + option;
+    fetch(url)
+        .then(res => res.json())
+        .then((out) => {
+            console.log('Checkout this JSON! ', out);
+
+            var oidata = document.getElementById("oidata");
+            var totalce = document.getElementById("totalce");
+            var totalpe = document.getElementById("totalpe");
+            var diffoi = document.getElementById("diffoi");
+            var pcr = document.getElementById("pcr");
+            let ce = 0, pe = 0;
+            out.resultData.data.forEach(function (element) {
+                ce = ce + element.calls_oi;
+                pe = pe + element.puts_oi;
+                ratio = ce / pe;
+            });
+            totalce.innerHTML = ce.toLocaleString('en-IN');
+            totalpe.innerHTML = pe.toLocaleString('en-IN');
+            diffoi.innerHTML = (ce - pe).toLocaleString('en-IN');
+            pcr.innerHTML = ratio.toPrecision(3);
+
+            var place = document.createElement("showData");
+            var table = document.createElement('table');
+
+            const list = out.resultData.data;
+            var col = [];
+            for (var i = 0; i < list.length; i++) {
+                for (var key in list[i]) {
+                    if (col.indexOf(key) === -1) {
+                        col.push(key);
+                    }
+                }
+            }
+
+            var tr = table.insertRow(-1);
+            for (var i = 0; i < col.length; i++) {
+                var th = document.createElement("th");
+                th.innerHTML = col[i];
+                tr.appendChild(th);
+            }
+            // add json data to the table as rows.
+            for (var i = 0; i < list.length; i++) {
+
+                tr = table.insertRow(-1);
+
+                for (var j = 0; j < col.length; j++) {
+                    var tabCell = tr.insertCell(-1);
+                    tabCell.innerHTML = list[i][col[j]];
+                }
+            }
+
+            var divShowData = document.getElementById('showData');
+            divShowData.innerHTML = "";
+            divShowData.appendChild(table);
+        })
+        .catch(err => { throw err });
+}
+runoi(option);
+
+
+
