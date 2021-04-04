4e3d66315b100bdfd75f08413ba156e796454430
sankethsj <32950017+sankethsj@users.noreply.github.com>
2021-04-04 16:11:51 +0530

    Enter

--- indexrange/range.js
+++ indexrange/range.js
@@ -1,3 +1,7 @@
+var ctx = document.getElementById('myChart');
+const strike = [];
+const calloi = [];
+const putoi = [];
 
 function changeoi(that) {
     if (that.value == 'niftyoichange') {
@@ -22,9 +26,63 @@ function runoi(option) {
                 ce = ce + element.calls_change_oi;
                 pe = pe + element.puts_change_oi;
             });
+            tce =ce;
+            tpe =pe;
+            doi = Math.abs(ce-pe);
             totalce.innerHTML = ce.toLocaleString('en-IN');
             totalpe.innerHTML = pe.toLocaleString('en-IN');
-            diffoi.innerHTML = (ce - pe).toLocaleString('en-IN');
+            diffoi.innerHTML = doi.toLocaleString('en-IN');
+            
+            var signal = document.getElementById('signal');
+            if( doi > 5000000 && tpe > tce ){
+                signal.innerText = "";
+                signal.innerHTML = "Bullish";
+                signal.style.color = 'var(--call)';
+
+            } else if( doi > 5000000 && tce > tpe){
+                signal.innerText = "";
+                signal.innerHTML = "Bearish";
+                signal.style.color = 'var(--put)';
+            } else{
+                signal.innerHTML = "Neutral";
+                signal.style.color = 'var(--font-color)';
+            }
+            
+            //chart code
+            let arr = out.resultData.data;       
+            for(let i=0; i< arr.length; i++){
+                strike[i] = arr[i].strike_price
+                calloi[i] = arr[i].calls_change_oi
+                putoi[i] = arr[i].puts_change_oi
+            }
+            var myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: strike,
+                    datasets: [{
+                        label: 'Change in Call OI',
+                        data: calloi,
+                        borderWidth: 1,
+                        backgroundColor: "rgba(0,255,0,0.7)",
+                    },
+                    {
+                        label: 'Change in Put OI',
+                        data: putoi,
+                        backgroundColor: "rgba(255,0,0,0.7)",
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    maintainAspectRatio:
+                        false,
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    },
+                }      
+            });
+
 
 
             var table = document.createElement('table');
@@ -70,6 +128,13 @@ document.getElementById('index').value = option;
 runoi(option);
 
 
+function showChart(e){
+    e.preventDefault();
+    document.getElementById('ccontainer').style.display = 'block';
+    
+}
 
-
-
+function hideChart(e){
+    e.preventDefault();
+    document.getElementById('ccontainer').style.display = 'none';
+}
