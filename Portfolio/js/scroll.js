850b1126ecc483fe97bee2947642a21ccdf89efd
stocksignal <noobtrader81@gmail.com>
2021-05-21 22:03:49 +0530

    v2

--- Portfolio/js/scroll.js
+++ Portfolio/js/scroll.js
@@ -48,3 +48,35 @@ setTimeout(() => {
             delay: (el, i) => 800 * i
         });
 }, 1000);
+
+setTimeout(() => {
+var textWrapper = document.querySelector('.ml16');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml16 .letter',
+    translateY: [-100,0],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 30 * i
+  });
+}, 2000);
+
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
