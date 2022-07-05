b8395b47bb282e10cd11837ee719616ad1095c37
sanketh <32950017+sankethsj@users.noreply.github.com>
2022-07-05 20:51:34 +0530

    refractored clock.js & updated favicons

--- clock/clock.js
+++ clock/clock.js
@@ -92,8 +92,7 @@ const digits = [
     }
 ];
 
-// function that rotates the hands
-function digit1(nmbr) {
+function display_digit(position, nmbr) {
     // get the current Date object from which we can obtain the current hour, minute and second
     let n = digits[nmbr];
     let digi = n[nmbr];
@@ -106,112 +105,57 @@ function digit1(nmbr) {
 
         // apply the rotate style to each element
         // use backtick `` instead of single quotes ''
-        let h11 = document.getElementById(`h1${count}`);
-        let m11 = document.getElementById(`m1${count}`);
+        let h11 = document.getElementById(`h${position}${count}`);
+        let m11 = document.getElementById(`m${position}${count}`);
         h11.style.transform = `rotate(${minutesRotate}deg)`;
         m11.style.transform = `rotate(${hoursRotate}deg)`;
         count++;
     });
 }
-function digit2(nmbr) {
-    // get the current Date object from which we can obtain the current hour, minute and second
-    let n = digits[nmbr];
-    let digi = n[nmbr];
-    count = 1;
-    digi.forEach(e => {
 
-        // actual deg to rotate
-        const minutesRotate = e.minute * 6;
-        const hoursRotate = e.hour * 30;
-
-        // apply the rotate style to each element
-        // use backtick `` instead of single quotes ''
-        let h11 = document.getElementById(`h2${count}`);
-        let m11 = document.getElementById(`m2${count}`);
-        h11.style.transform = `rotate(${minutesRotate}deg)`;
-        m11.style.transform = `rotate(${hoursRotate}deg)`;
-        count++;
-    });
-}
-function digit3(nmbr) {
-    // get the current Date object from which we can obtain the current hour, minute and second
-    let n = digits[nmbr];
-    let digi = n[nmbr];
-    count = 1;
-    digi.forEach(e => {
-
-        // actual deg to rotate
-        const minutesRotate = e.minute * 6;
-        const hoursRotate = e.hour * 30;
-
-        // apply the rotate style to each element
-        // use backtick `` instead of single quotes ''
-        let h11 = document.getElementById(`h3${count}`);
-        let m11 = document.getElementById(`m3${count}`);
-        h11.style.transform = `rotate(${minutesRotate}deg)`;
-        m11.style.transform = `rotate(${hoursRotate}deg)`;
-        count++;
-    });
-}
-function digit4(nmbr) {
-    // get the current Date object from which we can obtain the current hour, minute and second
-    let n = digits[nmbr];
-    let digi = n[nmbr];
-    count = 1;
-    digi.forEach(e => {
-
-        // actual deg to rotate
-        const minutesRotate = e.minute * 6;
-        const hoursRotate = e.hour * 30;
-
-        // apply the rotate style to each element
-        // use backtick `` instead of single quotes ''
-        let h11 = document.getElementById(`h4${count}`);
-        let m11 = document.getElementById(`m4${count}`);
-        h11.style.transform = `rotate(${minutesRotate}deg)`;
-        m11.style.transform = `rotate(${hoursRotate}deg)`;
-        count++;
-    });
-}
 // for every 1000 milliseconds(ie, 1 second) interval, activate the rotate() function.
-
-
 setInterval(() => {
     const currentDate = new Date();
-    let hr = currentDate.getHours();
-    let mn = currentDate.getMinutes();
-    let hrstr = hr.toString();
-    let mnstr = mn.toString();
-    if(mnstr.length<2){
-        mnstr = "0"+mnstr;
+    let hour = currentDate.getHours();
+    let minute = currentDate.getMinutes();
+    let hourStrng = hour.toString();
+    let minuteString = minute.toString();
+    if (minuteString.length < 2) {
+        minuteString = "0" + minuteString;
     }
-    digit1(hrstr.slice(0, 1));
-    digit2(hrstr.slice(1, 2));
-    digit3(mnstr.slice(0, 1));
-    digit4(mnstr.slice(1, 2));
+    let first_digit = hourStrng.slice(0, 1)
+    let second_digit = hourStrng.slice(1, 2)
+    let third_digit = minuteString.slice(0, 1)
+    let fourth_digit = minuteString.slice(1, 2)
+    
+    display_digit(1, first_digit);
+    display_digit(2, second_digit);
+    display_digit(3, third_digit);
+    display_digit(4, fourth_digit);
 }, 1000);
+
 
 //dark-mode code
 const toggleSwitch = document.querySelector('.fa-moon-o');
 const currentTheme = localStorage.getItem('theme');
 toggleSwitch.addEventListener('click', switchTheme);
-function switchTheme(e){
+function switchTheme(e) {
     const currentTheme = localStorage.getItem('theme');
-    if(currentTheme == 'light'){
+    if (currentTheme == 'light') {
         document.documentElement.setAttribute('data-theme', 'dark');
         localStorage.setItem('theme', 'dark');
     }
-    else if(currentTheme == 'dark') {
+    else if (currentTheme == 'dark') {
         document.documentElement.setAttribute('data-theme', 'light');
         localStorage.setItem('theme', 'light');
     }
 }
 
-if(currentTheme){
+if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
-    localStorage.setItem('theme',currentTheme);
+    localStorage.setItem('theme', currentTheme);
 }
-else{
+else {
     document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme','light');
+    localStorage.setItem('theme', 'light');
 }
